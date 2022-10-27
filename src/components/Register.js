f14e1edb074beca660e1995fa0cdd032d1a30066
harsh17112000 <77334392+harsh17112000@users.noreply.github.com>
2022-10-27 16:01:30 +0530

    done

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,30 +6,47 @@ import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
 
-    const {data,setData} = useContext(userContext);
+    const { data, setData } = useContext(userContext);
     console.log(data)
 
     const history = useNavigate();
 
-    const [inputValue,setInputValue] = useState({
-        fname:"",
-        date:"",
-        age:"",
-        gender:""
+    const [inputValue, setInputValue] = useState({
+        fname: "",
+        date: "",
+        age: "",
+        gender: "",
+        course: ""
     });
-
+    console.log(inputValue)
     // const [data,setData] = useState([])
 
-    const GetInputValue = (e)=>{
-        const {name,value} = e.target;
-        setInputValue({...inputValue,[name]:value})
+    const GetInputValue = (e) => {
+        const { name, value } = e.target;
+        setInputValue({ ...inputValue, [name]: value })
     }
 
-    const storeValue = (e)=>{
+    const storeValue = (e) => {
         e.preventDefault();
 
-        setData([...data,inputValue]);
-        history("/")
+        const { fname, date, age, gender, course } = inputValue;
+
+        if (fname == "") {
+            alert("fname is required")
+        } else if (date == "") {
+            alert("date is required")
+        } else if (age == "") {
+            alert("age is required")
+        } else if (gender == "") {
+            alert("gender is required")
+        } else if (course == "") {
+            alert("course is required")
+        } else if (age < 8) {
+            alert("age should be greter than 8")
+        } else {
+            setData([...data, inputValue]);
+            history("/")
+        }
     }
 
 
@@ -48,11 +65,37 @@ const Register = () => {
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasictext">
                         <Form.Label>gender</Form.Label>
-                        <Form.Control type="text" onChange={GetInputValue} name = "gender" value={inputValue.gender} />
+                        {/* <Form.Control type="radio" onChange={GetInputValue} name = "gender" value={inputValue.gender} />
+                        <Form.Control type="radio" onChange={GetInputValue} name = "gender" value={inputValue.gender} /> */}
+                        <Form.Check
+                            inline
+                            label="male"
+                            name="gender"
+                            type="radio"
+                            value="male"
+                            onChange={GetInputValue}
+                            id />
+                        <Form.Check
+                            inline
+                            label="female"
+                            value="female"
+                            name="gender"
+                            type="radio"
+                            onChange={GetInputValue}
+                            id />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Age</Form.Label>
-                        <Form.Control type="text" onChange={GetInputValue} name = "age" value={inputValue.age} />
+                        <Form.Control type="text" onChange={GetInputValue} name="age" value={inputValue.age} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Course</Form.Label>
+                        <Form.Select aria-label="Default select example" name="course" onChange={GetInputValue}>
+                            <option>Open this select menu</option>
+                            <option value="python">python</option>
+                            <option value="c++">c++</option>
+                            <option value="java">java</option>
+                        </Form.Select>
                     </Form.Group>
                     <Button variant="primary" onClick={storeValue} type="submit">
                         Submit
--- src/components/StudentDetails.js
+++ src/components/StudentDetails.js
@@ -10,35 +10,35 @@ const StudentDetails = () => {
 
     const { data, setData } = useContext(userContext);
 
+    
+    const history = useNavigate();
+
     const [show, setShow] = useState(false);
+    const [showupdate, setShowupdate] = useState(false);
+
+    const [userdltId, setUserDltId] = useState("");
+    const [userupdateId, setUserupdateId] = useState("");
 
     const [search, setSearch] = useState(data);
 
     const handleClosedlt = () => setShow(false);
     const handleShowdlt = () => setShow(true);
 
-    const handleCloseupdate = () => setShow(false);
-    const handleShowupdate = () => setShow(true);
+    const handleCloseupdate = () => setShowupdate(false);
+    const handleShowupdate = () => setShowupdate(true);
 
-    const [inputValue,setInputValue] = useState({
-        fname:"",
-        date:"",
-        age:"",
-        gender:""
+    const [inputValue, setInputValue] = useState({
+        fname: "",
+        date: "",
+        age: "",
+        gender: ""
     });
 
-    const [userdltId, setUserDltId] = useState("");
-    const [userupdateId, setUserupdateId] = useState("");
-    console.log(userupdateId)
 
-    const GetInputValue = (e)=>{
-        const {name,value} = e.target;
-        setInputValue({...inputValue,[name]:value})
+    const GetInputValue = (e) => {
+        const { name, value } = e.target;
+        setInputValue({ ...inputValue, [name]: value })
     }
-
-    console.log(inputValue)
-
-    const history = useNavigate();
 
     const registerPage = () => {
         history("/register")
@@ -48,36 +48,46 @@ const StudentDetails = () => {
         const dltuserdata = data.filter((el, index) => {
             return index !== userdltId
         });
-        setData(dltuserdata)
+        setSearch(dltuserdata)
     }
 
-//    update user
-const storeValue = (e)=>{
-    // e.preventDefault();
-    const userupdate = data.map((el,index)=>{
-        return index === userupdateId ? inputValue : el
-    });
-    console.log(userupdate)
-    setData(userupdate)
-}
+    //    update user
+    const storeValue = (e) => {
+        // e.preventDefault();
+        const userupdate = data.map((el, index) => {
+            return index === userupdateId ? inputValue : el
+        });
+        console.log(userupdate)
+        setSearch(userupdate)
+    }
 
-// search user
+    // search user
 
-    const searchuser = (e)=>{
+    const searchuser = (e) => {
         let letter = e.toLowerCase();
-        console.log(letter)
-        console.log(search)
 
-        if(letter === ""){
+        if (letter === "") {
             setSearch(data)
-        }else{
-            
-            const filtersearch = search.filter((el,index)=>{
-                return el.fname.toLowerCase().includes(letter)
+        } else {
+
+            const filtersearch = search.filter((el, index) => {
+                return el.fname.toLowerCase().match(letter)
             });
             setSearch(filtersearch)
         }
 
+    }
+
+
+    // filter gender
+    const filtergender = (e) => {
+
+        const { value } = e.target
+
+        const filterbygender = search.filter((el, ind) => {
+            return el.gender === value
+        })
+        setSearch(filterbygender)
     }
 
     return (
@@ -85,7 +95,14 @@ const storeValue = (e)=>{
             <div className="container">
                 <h1 className='text-center mt-2'>Student Details</h1>
                 <div className='text-start'>
-                    <input type="text" name="" onChange={(e)=>searchuser(e.target.value)} placeholder='search here' id="" />
+                    <input type="text" name="" onChange={(e) => searchuser(e.target.value)} placeholder='search here' id="" />
+                </div>
+                <div>
+                    <Form.Select aria-label="Default select example" name="course" onChange={filtergender}>
+                        <label htmlFor="">sort by gender</label>
+                        <option value="male">male</option>
+                        <option value="female">female</option>
+                    </Form.Select>
                 </div>
                 <div className='mt-4 text-end'>
                     <Button variant="primary" onClick={registerPage}>Add</Button>
@@ -155,33 +172,49 @@ const storeValue = (e)=>{
 
                     {/* edit modal */}
 
-                    <Modal show={show} onHide={handleCloseupdate}>
+                    <Modal show={showupdate} onHide={handleCloseupdate}>
                         <Modal.Header closeButton>
                             <Modal.Title>Are you sure you want to delete user</Modal.Title>
                         </Modal.Header>
                         <div className="container">
-                        
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="text"  value={inputValue.fname}  onChange={GetInputValue} name='fname' />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>date Of Birth</Form.Label>
-                                <Form.Control type="date" name="date" value={inputValue.date} onChange={GetInputValue} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasictext">
-                                <Form.Label>gender</Form.Label>
-                                <Form.Control type="text"  name="gender" value={inputValue.gender} onChange={GetInputValue} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Age</Form.Label>
-                                <Form.Control type="text"  name="age" value={inputValue.age}  onChange={GetInputValue}/>
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
+
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control type="text" value={inputValue.fname} onChange={GetInputValue} name='fname' />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>date Of Birth</Form.Label>
+                                    <Form.Control type="date" name="date" value={inputValue.date} onChange={GetInputValue} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasictext">
+                                    <Form.Label>gender</Form.Label>
+                                    {/* <Form.Control type="text" name="gender" value={inputValue.gender} onChange={GetInputValue} /> */}
+                                    <Form.Check
+                                        inline
+                                        label="male"
+                                        name="gender"
+                                        type="radio"
+                                        value="male"
+                                        checked={inputValue.gender == "male" ? true : false}
+                                        onChange={GetInputValue}
+                                        id />
+                                    <Form.Check
+                                        inline
+                                        label="female"
+                                        value="female"
+                                        name="gender"
+                                        type="radio"
+                                        checked={inputValue.gender == "female" ? true : false}
+                                        onChange={GetInputValue}
+                                        id />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Age</Form.Label>
+                                    <Form.Control type="text" name="age" value={inputValue.age} onChange={GetInputValue} />
+                                </Form.Group>
+
+                            </Form>
                         </div>
                         <Modal.Footer>
                             <Button variant="secondary" onClick={handleCloseupdate}>
@@ -195,9 +228,6 @@ const storeValue = (e)=>{
                             </Button>
                         </Modal.Footer>
                     </Modal>
-
-
-
                 </div>
             </div>
         </>
